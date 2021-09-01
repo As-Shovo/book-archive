e962ce3106c3ce6305ded3d813040f537df746da
As-Shovo <akbarshovo18@gmail.com>
2021-09-02 03:56:46 +0600

    mathaNosto

--- app.js
+++ app.js
@@ -2,27 +2,58 @@
 const inputField = document.getElementById('input-field');
 const searchButton = document.getElementById('button-search');
 const booksItems = document.getElementById('books-items');
+const showResultNumber = document.getElementById('show-result-number');
 
 // click Event Button Search
 
 searchButton.addEventListener('click', () => {
     const inputFieldValue = inputField.value;
-    // const url = ` http://openlibrary.org/search.json?q=${inputFieldValue}`;
-    const url = ` http://openlibrary.org/search.json?q=A Game of Thrones`;
+    const url = `https://openlibrary.org/search.json?q=${inputFieldValue}`;
+    // const url = ` https://openlibrary.org/search.json?q=javascript`;
+
+    //clear books Items Field
+    booksItems.textContent = '';
+
+    // Clear show rsult 
+    showResultNumber.innerHTML = '';
 
     // Function of Data Load 
     const libaryLoad = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => displayLibaryData(data))
+
     }
     libaryLoad();
+    // clear input field
     inputField.value = '';
+
+
 
     // Function of Data Display 
     const displayLibaryData = libaryDatas => {
-        console.log(libaryDatas.docs.length);
+        //clear books Items Field
+        // booksItems.textContent = '';
+
+        // // Clear show rsult 
+        // showResultNumber.innerHTML= '';
+
+
+        if (libaryDatas.numFound === 0) {
+            
+            return errorMessage();
+        };
+
+        console.log(libaryDatas.numFound);
         const libaryBooks = libaryDatas.docs;
+
+        showResultNumber.innerHTML = `
+            <div class="card m-auto p-3 mt-3 bg-success" style="width: 35rem">
+                <h5 class="card-title text-center">Dear Sir/Ma'am, Your Result Number is <b class="text-warning">-- ${libaryBooks.length} --</b></h5>
+            </div>
+        `;
+
+
 
         libaryBooks.forEach(libarybook => {
             // console.log(libarybook.cover_i);
@@ -47,6 +78,7 @@ searchButton.addEventListener('click', () => {
             `
                 booksItems.appendChild(div);
             }
+            // console.log('error');
 
 
 
@@ -54,3 +86,24 @@ searchButton.addEventListener('click', () => {
         });
     }
 })
+
+const errorMessage = () => {
+    const errorMessageDiv = document.getElementById("error-message");
+    // document.getElementById("p").innerHTML = "";
+    const valueInput = document.getElementById('input-field');
+    console.log(valueInput.value);
+        
+
+
+    errorMessageDiv.innerHTML = ` <div class="card m-auto p-5 mt-5 bg-warning" style="width: 18rem">
+          <h5 class="card-title">Dear Sir/Ma'am,</h5>
+          <p class="card-text">
+            Your search <b class="text-danger">-- ${valueInput.value} --</b> did not match Book Name. Please enter a
+            correct name.
+          </p>
+        </div>`;
+
+        // showResultNumber.innerText ='';
+}
+
+// errorMessage();
